478b924d123ec80d42133104a267cc7c0e5ef81b
DogDad65 <banton65@gmail.com>
2023-07-08 12:01:33 -0400

    Pytest

--- app/main.py
+++ app/main.py
@@ -6,9 +6,12 @@ from .database import engine
 from .routers import post, user, auth, vote
 from .config import settings
 
-origins = ["*"]   
+
 # models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,15 +20,16 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-       
+
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
 
+
 @app.get("/")
 def root():
-    return {"message": "Hello World! It Works!"}
+    return {"message": "Hello World"}
 
 
     
